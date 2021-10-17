8be382be2c9a9916dd051c4892a153aea4beda8f
vishal.Alok <you@example.com>
2021-10-17 21:31:38 +0530

    Pivate

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+// C# Program to illustrate calling
+// a Private constructor
+using System;
+namespace privateConstructorExample
+{
+    public class Geeks
+    {
+        private Geeks()
+        {
+        }
+        public static int count_geeks;
+        public static int geeks_Count()
+        {
+            return ++count_geeks;
+        }
+        public static void Main()
+        {
+            // Geeks s = new Geeks(); // Error
+            Geeks.count_geeks = 99;
+            Geeks.geeks_Count();
+        }
+    }
+}
